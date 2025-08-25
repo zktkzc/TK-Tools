f428b5b230aad1b64be51b8fa8e5f3aed7093e40
tkzc00 <2238983030@qq.com>
2025-08-25 22:04:29 +0800

    添加Footer

--- src/renderer/src/components/Footer.vue
+++ src/renderer/src/components/Footer.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="w-full h-[30px]">Footer</div>
+  <div
+    class="w-full h-[30px] text-sm leading-[30px] text-center bg-gray-200 dark:bg-gray-500 dark:text-slate-200 rounded-bl-md rounded-br-md"
+    style="user-select: none"
+  >
+    tkzc00作品
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
