79a305cd60239169ff9ad09298e0b99676541bd1
tkzc00 <2238983030@qq.com>
2025-09-07 00:18:47 +0800

    调整日期计算器页面的布局样式

--- src/renderer/src/components/Card.vue
+++ src/renderer/src/components/Card.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="border dark:border-[#4C4D4F] rounded-md overflow-hidden">
+  <div class="border dark:border-[#4C4D4F] rounded-md">
     <div
       class="text-[#515A6E] dark:text-[#AEB9C0] dark:bg-[#333333] border-b dark:border-[#4C4D4F] p-2 text-sm"
       style="user-select: none"
